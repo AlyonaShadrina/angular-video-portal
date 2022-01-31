01f8fd8a6bc793022902f12f8f2c3deec4d547d5
Shadrina Alyona <alyon.shadrina@gmail.com>
2022-01-31 15:22:07 +0200

    feat: add linter, components, interfaces

---
+++ src/shared/interfaces.ts
@@ -0,0 +1,13 @@
+export interface ICourse {
+  id: number;
+  title: string;
+  creation_date: string;
+  duration: number;
+  description: string;
+}
+
+export interface IUser {
+  id: number;
+  first_name: string;
+  last_name: string;
+}
